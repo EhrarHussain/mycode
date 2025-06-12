f36fe4d58ae488540b5e45b77ea88448eff84249
EhrarHussain <61885647+EhrarHussain@users.noreply.github.com>
2025-06-12 12:53:07 +0500

    Update backup-zip.sh

--- backup-zip.sh
+++ backup-zip.sh
@@ -2,9 +2,11 @@
 
 app_sys_user=$(basename "$(dirname "$PWD")")
 
+echo "Starting"
+
 mysqldump "$app_sys_user" > "${app_sys_user}.sql"
 
-zip -r "backup-${app_sys_user}.zip" .
+zip -r "backup-${app_sys_user}.zip" . -x "backup-zip.sh"
 
 chown "$app_sys_user":www-data backup*
 
